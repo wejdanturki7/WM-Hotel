1c5fa4a7077197869a91c2e30c35d533c3799256
Wejdan <Wejdantaltaibi@gmail.com>
2023-08-24 11:46:47 +0300

    extension & ui classes Donee

---
+++ wm_hotel/lib/utilities/ui_classes/appcolor.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color primaryColor = const Color(0xff0079FF); // dark blue
+  static Color secondaryColor = const Color(0xff79E0EE); //light blue
+  static Color whiteColor = Colors.white;
+  static Color blackColor = const Color(0xff000000);
+  static Color greyColor = const Color(0xff424242);
+}
