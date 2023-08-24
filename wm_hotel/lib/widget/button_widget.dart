53523823ba81d3d8f55a208c0d59899952b48ac0
dev one <dev@test.com>
2023-08-24 19:55:58 +0300

    add parameter for button widget

--- wm_hotel/lib/widget/button_widget.dart
+++ wm_hotel/lib/widget/button_widget.dart
@@ -4,8 +4,10 @@ import 'package:wm_hotel/utilities/ui_classes/appcolor.dart';
 class CustomButton extends StatelessWidget {
   const CustomButton({
     super.key,
+    required this.buttonTitle,
   });
 
+  final String buttonTitle;
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -21,10 +23,10 @@ class CustomButton extends StatelessWidget {
               ],
             ),
             borderRadius: BorderRadius.circular(25.0)),
-        child: const Text(
-          'Button name',
+        child: Text(
+          buttonTitle,
           textAlign: TextAlign.center,
-          style: TextStyle(color: Colors.white, fontSize: 20.0),
+          style: const TextStyle(color: Colors.white, fontSize: 20.0),
         ),
       ),
     );
