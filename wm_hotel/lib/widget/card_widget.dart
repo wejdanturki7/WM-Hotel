1fb901b7d5d455548f6b6e2d5eee23f329c869cb
dev one <dev@test.com>
2023-08-24 12:54:55 +0300

    add card widget

---
+++ wm_hotel/lib/widget/card_widget.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+class CardWidget extends StatelessWidget {
+  const CardWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Hello World!'),
+    );
+  }
+}
