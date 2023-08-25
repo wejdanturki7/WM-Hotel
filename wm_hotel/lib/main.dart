fad9aec96655ebff2841934730e011b0ab197ac2
dev one <dev@test.com>
2023-08-25 22:55:39 +0300

    Home page created, & need some updates

--- wm_hotel/lib/main.dart
+++ wm_hotel/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wm_hotel/screens/hotel_detiles_screen.dart';
+import 'package:wm_hotel/screens/home_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +11,8 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: HoteleDetilesScreen(),
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
     );
   }
 }
---
+++ wm_hotel/lib/widget/home_screen_widgets/titled_city_icon.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:wm_hotel/utilities/ui_classes/spaces.dart';
+
+class CityIcon extends StatelessWidget {
+  const CityIcon({
+    super.key,
+    required this.imgUrl,
+    required this.city,
+  });
+
+  final String imgUrl;
+  final String city;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        CircleAvatar(
+          radius: 35,
+          backgroundImage: AssetImage(
+            imgUrl,
+          ),
+        ),
+        Spaces.spaceH4,
+        Text(city),
+      ],
+    );
+  }
+}
